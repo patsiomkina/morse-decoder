38d88c85071b5e22b1c94062f22f50b7f89bba0d
patsiomkina <patsiomkina@inbox.ru>
2023-02-10 17:05:35 +0300

    feat: add solution for 'morse' task

--- src/index.js
+++ src/index.js
@@ -38,9 +38,48 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+	let wordArr = [];
+	let startMorse = [];
+	let morse = [];
+	let resultArr = [];
+
+	for (let i = 0; i < expr.length; i = i + 10) {
+		let slice = expr.slice(i, i + 10);
+		if (slice[0] === '*') {
+			startMorse.push([' ']);
+		} else {
+			for (let i = 0; i < 10; i = i + 2) {
+				if (slice[i] === '1') {
+					if (slice.slice(i, i + 2) === '10') {
+						wordArr.push('.');
+					} else if (slice.slice(i, i + 2) === '11') {
+						wordArr.push('-');
+					}
+				}
+			}
+			startMorse.push(wordArr);
+			wordArr = [];		
+		}
+	}
+
+	for (let arr of startMorse) {
+		morse.push(arr.join(''));
+	}
+
+	for (let item of morse) {
+		if (item === ' ') {
+			resultArr.push(' ');
+		} else {
+			for (const [key, value] of Object.entries(MORSE_TABLE)) {
+				if (item === key) {
+					resultArr.push(value);
+				} 
+			}
+		}
+	}
+	return resultArr.join('');
 }
 
 module.exports = {
-    decode
+  decode
 }
